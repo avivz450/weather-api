ecf28ed9f751b7d4929ba6541a914741cd7aaff0
Aviv Zafrani <avivzafrani@Avivs-MacBook-Pro.local>
2022-02-03 17:03:51 +0200

    Modified lint + prettier configuration

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import log from '@ajar/marker';
-import app from './modules/app/app.js';
+import log from "@ajar/marker";
+import app from "./modules/app/app.js";
 
-const { PORT = 3030, HOST = 'localhost' } = process.env;
+const { PORT = 3030, HOST = "localhost" } = process.env;
 
 (async () => {
     await app.listen(PORT as number, HOST as string);
-    log.magenta(`api is live on`, ` ✨ ⚡  http://${HOST}:${PORT} ✨ ⚡`);
+    log.magenta("api is live on", ` ✨ ⚡  http://${HOST}:${PORT} ✨ ⚡`);
 })().catch(console.log);
--- src/modules/app/app.ts
+++ src/modules/app/app.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-//import morgan from "morgan";
-import cors from 'cors';
+import express from "express";
+// import morgan from "morgan";
+import cors from "cors";
 
-//import { errorResponseMiddleware, logErrorMiddleware, printErrorMiddleware, notFoundMiddleware } from "../../middlewares/errors.handler.js";
-//import { logHttpMiddleware } from "../../middlewares/http.logger.js";
+// import { errorResponseMiddleware, logErrorMiddleware, printErrorMiddleware, notFoundMiddleware } from "../../middlewares/errors.handler.js";
+// import { logHttpMiddleware } from "../../middlewares/http.logger.js";
 
 class App {
     public app: express.Application;
@@ -18,25 +18,26 @@ class App {
 
     private configGeneral() {
         this.app.use(cors());
-        //  this.app.use(morgan("dev"));
+        //   this.app.use(morgan("dev"));
         this.app.use(express.json());
     }
 
     private configRoutes() {}
 
     private configMiddlewares() {
-        const { HTTP_LOG_PATH = './logs/users.http.log' } = process.env;
+        const { HTTP_LOG_PATH = "./logs/users.http.log" } = process.env;
 
-        //  this.app.use(logHttpMiddleware(HTTP_LOG_PATH));
+        //   this.app.use(logHttpMiddleware(HTTP_LOG_PATH));
     }
 
     private configErrorHandlers() {
-        const { ERRORS_LOG_PATH = './logs/users.errors.log' } = process.env;
+        const { ERRORS_LOG_PATH = "./logs/users.errors.log" } = process.env;
 
-        // this.app.use(logErrorMiddleware(ERRORS_LOG_PATH));
-        // this.app.use(printErrorMiddleware);
-        // this.app.use(errorResponseMiddleware);
-        // this.app.use(notFoundMiddleware);
+        //  this.app.use(logErrorMiddleware(ERRORS_LOG_PATH));
+        //  this.app.use(printErrorMiddleware);
+        //  this.app.use(errorResponseMiddleware);
+        //  this.app.use(notFoundMiddleware);
+        //  hhghg
     }
 }
 export default new App().app;
