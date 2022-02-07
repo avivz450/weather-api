c7047f7f5fb0c2b684df090aa4625c82f2ea28b3
yair cohen <yaircohens89@gmail.com>
2022-02-07 13:21:57 +0200

    modified types

--- src/types/account.types.ts
+++ src/types/account.types.ts
@@ -1,5 +1,5 @@
 export interface IAccount {
-    accountID: number;
+    account_id: number;
     currency: number;
     balance?: number;
     status?: AccountStatuses;
@@ -10,11 +10,19 @@ export enum AccountStatuses {
     active,
 }
 
-export interface IIndividualAccount extends Account {
-    individualID: number;
-    firstName: string;
-    lastName: string;
+export interface IIndividualAccount extends IAccount {
+    individual_id: number;
+    first_name: string;
+    last_name: string;
     email?: string;
+    address?: IAddress;
+}
+
+export interface IBusinessAccount extends Account {
+    account_id: number;
+    company_id: string;
+    company_name: string;
+    context?: string;
     address?: IAddress;
 }
 
@@ -27,4 +35,17 @@ export interface IAddress {
     region: string;
     street_name: string;
     street_number: number;
+}
+
+export interface ITransferRequest {
+    sourceAccount: number,
+    DestinationAccount: number,
+    currency?: string,
+    amount: number,
+
+}
+
+export interface ITransferResponse {
+    sourceAccount: Partial<IAccount>,
+    DestinationAccount: Partial<IAccount>
 }
