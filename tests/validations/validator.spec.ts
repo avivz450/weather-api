e9c14ab8b51ae7d24e8820478d2e9b4af4d374f8
Renana Arfi <renanaa@rapyd.ac>
2022-02-13 16:11:49 +0200

    sdsdds

--- tests/validations/validator.spec.ts
+++ tests/validations/validator.spec.ts
@@ -4,13 +4,12 @@ import PubSub from 'pubsub-js';
 import validator from '../../src/utils/validator.js';
 import { IGeneralObj } from '../../src/types/general.types.js';
 
+
 describe('#validator module', function () {
   context('check Required Fields Exist', () => {
     it('should exists', () => {
       // @ts-ignore
       expect(validator.checkRequiredFieldsExist()).to.be.a('function');
-      // @ts-ignore
-      expect(validator.checkRequiredFieldsExist()).to.be.instanceOf(Function);
     });
 
     it('should return true if all required fields exist', () => {
