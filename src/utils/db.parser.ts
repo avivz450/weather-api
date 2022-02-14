f5604923ac3b6bf79f236f862a2ca01ec1a31c98
Aviv Zafrani <avivzafrani@Avivs-MacBook-Pro.local>
2022-02-15 01:33:33 +0200

    deleted commited code

--- src/utils/db.parser.ts
+++ src/utils/db.parser.ts
@@ -67,29 +67,6 @@ export function parseIndividualAccountQueryResult(query_result_obj: IIndividualA
   } as unknown as IIndividualAccount;
 }
 
-export function parseBusinessAccountQueryResult(query_result_obj: IBusinessAccountDB) {
-  const { accountID, currencyCode, balance, statusName, companyID, companyName, context, addressID, countryCode, countryName, postalCode, city, region, streetName, streetNumber } = query_result_obj;
-
-  return {
-    account_id: accountID,
-    currency: currencyCode,
-    balance,
-    status: statusName,
-    company_id: companyID,
-    company_name: companyName,
-    context,
-    address: {
-      address_id: addressID,
-      country: countryCode,
-      country_name: countryName,
-      postal_code: postalCode,
-      city,
-      region,
-      street_name: streetName,
-      street_number: streetNumber,
-    },
-  } as unknown as IBusinessAccount;
-}
 
 export function parseBusinessAccountsQueryResult(query_result: IBusinessAccountDB[]) {
   return (query_result as IBusinessAccountDB[]).map(business_account => {
