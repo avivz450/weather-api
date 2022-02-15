b5da6cce0d067f7f8ea93e573290e915901bc4a5
yair cohen <yaircohens89@gmail.com>
2022-02-15 02:42:30 +0200

    all-running

--- src/repositories/Authenication.Repository.ts
+++ src/repositories/Authenication.Repository.ts
@@ -1,17 +1,18 @@
 import { OkPacket, RowDataPacket } from 'mysql2';
 import { sql_con } from '../db/sql/sql.connection.js';
 import DatabaseException from '../exceptions/db.exception.js';
+import { IAuthenticationQueryRes } from '../types/athentication.types.js';
 
 class AuthenticationRepository {
   async getSecretKeyAndAgentIdByAccessKey(accesKey: string) {
     try {
-      const query = `SELECT secretKey 
+      const query = `SELECT agentID, secretKey 
                             FROM agent
                             WHERE accessKey = ?`;
       const [secret_query_res] = (await sql_con.query(query, [accesKey])) as RowDataPacket[][];
-      const { secretKey } = secret_query_res[0];
+      const { secretKey, agentID } = secret_query_res[0];
 
-      return secretKey ? secretKey : null;
+      return secretKey && agentID ? { secret_key: secretKey, agent_id: agentID } as IAuthenticationQueryRes : null;
     } catch (err) {
       throw new DatabaseException('Failed to authenticate request`');
     }
